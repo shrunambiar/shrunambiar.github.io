04b47956cf65fa358c0c1cb259b1763a99501f3e
Anuja Golechha <Anuja@Anujas-Mac.local>
2018-02-26 16:42:02 -0600

    updated classify, whitelist, blacklist

--- Master/classify.py
+++ Master/classify.py
@@ -31,15 +31,8 @@ import numpy as np
 # dftrain = pd.read_csv('distancetoverbtrain.csv')
 # dftest = pd.read_csv('distancetoverbtest.csv')
 
-dftest = pd.read_csv('classifier_input_train.csv')
-dftrain = pd.read_csv('classifier_input_test.csv')
-
-
-# features = ['n-gram', 'PRE_DIST_VERB', 'POST_DIST_VERB', 'PRE_DIST_STOPWORD', 'POST_DIST_STOPWORD', 'PrecedingTitle', 'Apostrophe', 'PRE_DIST_FROM_THE', 'PRE_DIST_FROM_POSITION']
-
-# features = ['PrecedingTitle', 'n-gram']
-
-# features = ['PRE_DIST_VERB', 'POST_DIST_VERB', 'PRE_DIST_STOPWORD', 'POST_DIST_STOPWORD', 'PrecedingTitle', 'Apostrophe', 'PRE_DIST_FROM_THE', 'PRE_DIST_FROM_POSITION']
+dftrain = pd.read_csv('classifier_input_train.csv')
+dftest = pd.read_csv('classifier_input_test.csv')
 
 features = ['n-gram', 'PRE_DIST_VERB', 'POST_DIST_VERB', 'PrecedingTitle', 'PRE_DIST_FROM_THE', 'PRE_DIST_FROM_POSITION', 'NEGATIVE_FEATURE', 'POSITIVE_FEATURE', 'Surrounding_Caps','Apostrophe', 'POST_IS_PREPOSITION', 'RELATIONSHIP', 'POST_IS_SPEAK_VERB', 'PRE_IS_SPEAK_VERB', 'INSTANCE_IS_POSITION','INSTANCE_IS_COUNTRY']
 
@@ -52,7 +45,7 @@ target = dftrain['classtype'].as_matrix()
 fpl = []
 fnl = []
 
-X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.1)
+# X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.1)
 model = DecisionTreeClassifier()
 
 model.fit(data, target)
@@ -79,23 +72,50 @@ df.to_csv("fpl2.csv")
 df = pd.DataFrame(np.array(fnl))
 df.to_csv("fnl2.csv")
 
-
-newmodel = SelectFromModel(model)
-
-clf = Pipeline([
-  ('feature_selection', newmodel),
-  ('classification', DecisionTreeClassifier())
-  ])
-clf.fit(data, target)
-
 l = zip(features, model.feature_importances_)
 l.sort(key = lambda x:x[1], reverse=True)
 print l
+whitelist = ['Tony Blair', 'Alan Milburn', 'Charles Kennedy', 'David Blunkett', 'George Bush', 'Gordon Brown'] # 64
+
+whitelist = ['Tony Blair', 'Alan Milburn', 'Charles Kennedy', 'David Blunkett', 'George Bush', 'Gordon Brown',
+'Michael Howard'] # 66
+
+whitelist = ['Tony Blair', 'Alan Milburn', 'Charles Kennedy', 'David Blunkett', 'George Bush', 'Gordon Brown',
+'Michael Howard']
+
+whitelist2 = [w + "'s" for w in whitelist]
+
+blacklist = ['BBC']
+
+for i in range(n):
+    if predicted[i] == False and (dftest.iloc[i]['All-Words'] in whitelist or dftest.iloc[i]['All-Words'] in whitelist2):
+        predicted[i] = True
+    elif predicted[i] == True and dftest.iloc[i]['All-Words'] in blacklist:
+        predicted[i] = False
 
 
-expected = dftest['classtype'].as_matrix()
-
-predicted = clf.predict(dftest[features].as_matrix())
-# summarize the fit of the model
 print(metrics.classification_report(expected, predicted))
 print(metrics.confusion_matrix(expected, predicted))
+
+
+
+
+
+
+# newmodel = SelectFromModel(model)
+#
+# clf = Pipeline([
+#   ('feature_selection', newmodel),
+#   ('classification', DecisionTreeClassifier())
+#   ])
+# clf.fit(data, target)
+#
+#
+#
+#
+# expected = dftest['classtype'].as_matrix()
+#
+# predicted = clf.predict(dftest[features].as_matrix())
+# # summarize the fit of the model
+# print(metrics.classification_report(expected, predicted))
+# print(metrics.confusion_matrix(expected, predicted))
