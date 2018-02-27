40d0abeb0b4e9c3347d9de886816eb3ad82ad12a
Shruthi Kukal Nambiar <shruthinambiar@gmail.com>
2018-02-26 22:03:35 -0600

    updating project site

--- Master/classify.py
+++ Master/classify.py
@@ -12,13 +12,14 @@ def shuffle_in_unison(a, b):
 # Logistic Regression
 from sklearn import datasets
 from sklearn import metrics
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression,LinearRegression
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn import tree
 
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 from sklearn.pipeline import Pipeline
 
@@ -46,7 +47,7 @@ fpl = []
 fnl = []
 
 # X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.1)
-model = SVC()
+model = LinearRegression()
 
 model.fit(data, target)
 print(model)
@@ -77,21 +78,20 @@ df.to_csv("fnl2.csv")
 # print l
 whitelist = ['Tony Blair', 'Alan Milburn', 'Charles Kennedy', 'David Blunkett', 'George Bush', 'Gordon Brown'] # 64
 
-whitelist = ['Tony Blair', 'Alan Milburn', 'Charles Kennedy', 'David Blunkett', 'George Bush', 'Gordon Brown',
-'Michael Howard'] # 66
+whitelist = ['Tony Blair','Gordon Brown'] # 66
 
-whitelist = ['Tony Blair', 'Alan Milburn', 'Charles Kennedy', 'David Blunkett', 'George Bush', 'Gordon Brown',
-'Michael Howard']
+# whitelist = ['Tony Blair', 'Alan Milburn', 'Charles Kennedy', 'David Blunkett', 'George Bush', 'Gordon Brown',
+# 'Michael Howard']
 
 whitelist2 = [w + "'s" for w in whitelist]
 
-blacklist = ['BBC']
+# blacklist = ['BBC']
 
 for i in range(n):
     if predicted[i] == False and (dftest.iloc[i]['All-Words'] in whitelist or dftest.iloc[i]['All-Words'] in whitelist2):
         predicted[i] = True
-    elif predicted[i] == True and dftest.iloc[i]['All-Words'] in blacklist:
-        predicted[i] = False
+    # elif predicted[i] == True and dftest.iloc[i]['All-Words'] in blacklist:
+    #     predicted[i] = False
 
 
 print(metrics.classification_report(expected, predicted))
