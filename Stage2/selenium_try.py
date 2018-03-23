516f9872bca55252f90f2321750321c8a877a602
Ushmal Ramesh <ushmal.ramesh@gmail.com>
2018-03-23 14:55:59 -0500

    flipkart smartwatch extractor

--- Stage2/selenium_try.py
+++ Stage2/selenium_try.py
@@ -17,11 +17,11 @@ def lookup(driver, url):
 
     try:
         timeout = 5
-        j=83
-        while(j<175):
-            # driver.get(url)
+        j=0
+        while(j<41):
             url=driver.current_url
-            selector = "div[class=\"_3wU53n\"]"
+            driver.get(url)
+            selector = "div[class=\"_3BTv9X\"]"
             links = driver.find_elements_by_css_selector(selector)
             while (len(links)==0):
                 driver.find_element_by_class("_2AkmmA _2QL3XM _3W4trT _1eFTEo").click()
@@ -37,6 +37,7 @@ def lookup(driver, url):
                     pass
                 time.sleep(1)
                 while len(driver.window_handles)==1:
+                    driver.get(url)
                     links = driver.find_elements_by_css_selector(selector)
                     links[i].click()
                     print "I'm waiting for the window"
@@ -51,7 +52,7 @@ def lookup(driver, url):
                 string=driver.page_source
                 driver.close()
                 string=string.encode('utf-8')
-                f = open("./flipkart/output_" +str(i)+"_"+ str(j)+".html",'w')
+                f = open("./flipkart/smartwatches_" +str(i)+"_"+ str(j)+".html",'w')
                 f.write(string)
                 f.close()
                 # sourcecode.append(string)
@@ -75,7 +76,7 @@ def lookup(driver, url):
  
 if __name__ == "__main__":
     driver = init_driver()
-    url="https://www.flipkart.com/search?as=off&as-show=on&otracker=start&page=84&q=cellphone&viewType=list"
+    url="https://www.flipkart.com/search?as=off&as-show=on&otracker=start&page=1&q=smartwatches&viewType=list"
     driver.get(url)
     selector = "div[class=\"_3wU53n\"]"
     links=driver.find_elements_by_css_selector(selector)
