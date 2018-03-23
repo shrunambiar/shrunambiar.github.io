77fa132097b26c202937719f41b7ae5e204f1f85
Ushmal Ramesh <ushmal.ramesh@gmail.com>
2018-03-23 18:21:32 -0500

    working flipkart

--- Stage2/selenium_try.py
+++ Stage2/selenium_try.py
@@ -1,7 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
@@ -21,10 +21,10 @@ def lookup(driver, url):
         while(j<41):
             url=driver.current_url
             driver.get(url)
-            selector = "div[class=\"_3BTv9X\"]"
+            selector = "div[class=\"_3wU53n\"]"
             links = driver.find_elements_by_css_selector(selector)
             while (len(links)==0):
-                driver.find_element_by_class("_2AkmmA _2QL3XM _3W4trT _1eFTEo").click()
+                driver.find_element_by_class(selector).click()
                 links = driver.find_elements_by_css_selector(selector)
             print links     
             for i in range(0,len(links)):
